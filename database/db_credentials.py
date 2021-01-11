e805986eac589a159897bf8fac11285dc2d4d060
George Kochera <kocherag@oregonstate.edu>
2021-01-10 23:01:20 -0500

    provided a credential set for OSU and local development

--- database/db_credentials.py
+++ database/db_credentials.py
@@ -4,7 +4,16 @@
 # the following will be used by the webapp.py to interact with the database
 # You can also use environment variables
 
-host = 'classmysql.engr.oregonstate.edu'    #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-user = '<your-cs-340-db-username-here'      #CHANGE ME
-passwd = '<your-password-here>'             #CHANGE ME
-db = 'bsg'                                  
+# For Local Devlelopment
+host = 'localhost'
+user = 'root'                                   # can be different if you set up another username in your MySQL installation
+passwd = 'nqdarQOD^s!n}=Iy'                     
+db = 'bsg'
+
+
+# For OSU Flip Servers
+
+# host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
+# user = '<your-cs-340-db-username-here>'       # don't forget the CS_340 prefix
+# passwd = '<your-password-here>'               # should only be 4 digits if default
+# db = '<name-of-database-on-osu-server>'                                  
