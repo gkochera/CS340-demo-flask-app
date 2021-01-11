01b046008da1e7059afb99e89a6cf3bb79f68afb
George Kochera <kocherag@oregonstate.edu>
2021-01-10 22:41:59 -0500

    imported db_connector.py and db_credentials.py frrom mloprestas repo

---
+++ database/db_credentials.py
@@ -0,0 +1,10 @@
+# To actually have your app use this file, you need to RENAME the file to db_credentials.py
+# You will find details about your CS340 database credentials on Canvas.
+
+# the following will be used by the webapp.py to interact with the database
+# You can also use environment variables
+
+host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+user = 'cs340_laprestm' #CHANGE ME
+passwd = '3981' #CHANGE ME
+db = 'cs340_laprestm' #CHANGE ME
