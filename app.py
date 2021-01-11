9c45c25ce7a8d6002ac87f9aa9c500b8c4c5c7a1
George Kochera <kocherag@oregonstate.edu>
2021-01-10 20:17:12 -0500

    added a list of dictionaries for the template demo

--- app.py
+++ app.py
@@ -7,18 +7,40 @@ app = Flask(__name__)
 
 # Temporary
 
-person1 = {
+people =
+[
+{
     "name": "Thomas",
     "age": 33,
     "location": "New Mexico",
     "favorite_color": "Blue"
+},
+{
+    "name": "Gregory",
+    "age": 41,
+    "location": "Texas",
+    "favorite_color": "Red"
+},
+{
+    "name": "Vincent",
+    "age": 27,
+    "location": "Ohio",
+    "favorite_color": "Green"
+},
+{
+    "name": "Alexander",
+    "age": 29,
+    "location": "Florida",
+    "favorite_color": "Orange"
 }
+] 
+
 
 # Routes 
 
 @app.route('/')
 def root():
-    return render_template("main.j2")
+    return render_template("main.j2", people=people)
 
 # Listener
 
