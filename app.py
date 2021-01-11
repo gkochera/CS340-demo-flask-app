7651aa1cbd79faabea5438106147159597eced4e
George Kochera <kocherag@oregonstate.edu>
2021-01-10 23:02:20 -0500

    imported db_connector and db_creds

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+import database.db_connector as db
+import database.db_credentials as db_creds
 import os
 
 # Configuration
 
 app = Flask(__name__)
+db_connection = db.connect_to_database()
+
 
 # Temporary
 
